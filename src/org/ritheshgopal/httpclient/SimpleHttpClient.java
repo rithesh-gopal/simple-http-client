2a555c318a089a871d9d393e67aff27a376c495b
rithesh-gopal <rithesh.gopal@yahoo.com>
2018-04-15 13:17:15 +0530

    Sysouts to print response details

--- src/org/ritheshgopal/httpclient/SimpleHttpClient.java
+++ src/org/ritheshgopal/httpclient/SimpleHttpClient.java
@@ -12,9 +12,10 @@ public class SimpleHttpClient {
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		
 		String host = "127.0.0.1";
-		int port = 8000;
+		int port = 8081;
 		
 		//Create a TCP socker and connect to the host:port
+		System.out.println("Creating socket...");
 		Socket socket = new Socket(host, port);
 		
 		//Create the input and output streams for the network socket.
@@ -22,9 +23,13 @@ public class SimpleHttpClient {
 		PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
 		
 		//Sent request to the HTTP server.
-		out.println("POST /getUsers HTTP/1.1");
+		System.out.println("Sending request to server:");
+		System.out.println("Host: "+host+" Port: "+port);
+		out.println("GET / HTTP/1.1");
 		out.println(); // blank line separating header & body
 		out.flush();
+		System.out.println("Request sent.. Waiting for response..");
+		
 		//Ready the response and display on console.
 		String line;
 		// readLine() returns null if server close the network socket.
@@ -32,7 +37,7 @@ public class SimpleHttpClient {
 			System.out.println(line);
 		}
 		if(in.readLine() == null)
-			System.out.println("Connection to server lost");
+			System.out.println("Connection to server closed..");
 		//Close the I/O streams.
 		in.close();
 		out.close();
